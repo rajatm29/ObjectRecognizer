c7bc455db6a645f64a95fb494417f29d897cf55f
Rajat Monga <rajatmonga@Rajats-MacBook-Pro.local>
2017-12-31 14:52:29 -0600

    first commit

---
+++ ObjectRecognitionCoreML/ViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  ObjectRecognitionCoreML
+//
+//  Created by Rajat Monga on 12/31/17.
+//  Copyright © 2017 Rajat Monga. All rights reserved.
+//
+
+import UIKit
+import CoreML
+import AVKit
+import Vision
+
+
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .photo
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        
+        captureSession.addInput(input)
+        
+        captureSession.startRunning()
+        
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(previewLayer)
+        previewLayer.frame = view.frame
+        
+        let dataOutput = AVCaptureVideoDataOutput()
+        
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        
+        captureSession.addOutput(dataOutput)
+        
+        
+    
+        //VNImageRequestHandler(CGImage: CGImage, options[:]).perform(requests: [VNRequest])
+        
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didDrop sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        //print("camera was able to capture a frame", Date())
+        
+        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+    
+        guard let model = try? VNCoreMLModel(for: Resnet50().model) else { return }
+        let request = VNCoreMLRequest(model: model) {
+            (finishedReq, err) in
+            print(finishedReq.results)
+            
+            
+            
+            guard let results = finishedReq.results as? [VNClassificationObservation] else { return }
+            
+            guard let firstObservation = results.first else { return }
+            
+            print(firstObservation.identifier, firstObservation.confidence)
+            
+            
+        }
+            
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
