407e57fb1c7d635ec21ba61921905e4b4ab9e7a3
Rajat Monga <rajatmonga29@gmail.com>
2018-01-02 23:03:52 -0600

    Update ViewController.swift

--- ObjectRecognitionCoreML/ViewController.swift
+++ ObjectRecognitionCoreML/ViewController.swift
@@ -25,22 +25,16 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
         
         captureSession.addInput(input)
-        
         captureSession.startRunning()
-        
         
         let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
         view.layer.addSublayer(previewLayer)
         previewLayer.frame = view.frame
         
         let dataOutput = AVCaptureVideoDataOutput()
-        
         dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
         
         captureSession.addOutput(dataOutput)
-        
-        
-    
         //VNImageRequestHandler(CGImage: CGImage, options[:]).perform(requests: [VNRequest])
         
     }
@@ -49,32 +43,19 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         //print("camera was able to capture a frame", Date())
         
         guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
-    
         guard let model = try? VNCoreMLModel(for: Resnet50().model) else { return }
         let request = VNCoreMLRequest(model: model) {
             (finishedReq, err) in
-            print(finishedReq.results)
-            
-            
-            
+            //print(finishedReq.results)
             guard let results = finishedReq.results as? [VNClassificationObservation] else { return }
-            
-            guard let firstObservation = results.first else { return }
-            
-            print(firstObservation.identifier, firstObservation.confidence)
-            
-            
-        }
-            
+            guard let firstObservation = results.first else { return }    
+            print(firstObservation.identifier, firstObservation.confidence)                 
+        }    
         try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
     }
-    
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
